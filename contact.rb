1534448b02b97642f64409da8ba6ca356467bab1
vagrant <vagrant@precise64.(none)>
2013-06-11 20:37:03 +0000

    restarted TDD, by testing contact initialize

---
+++ contact.rb
@@ -0,0 +1,15 @@
+class Contact
+
+attr_accessor :first_name, :last_name, :email, :notes, :id
+
+	def initialize(first_name, last_name, email, notes, id)
+		@first_name = first_name
+		@last_name = last_name
+		@email = email
+		@notes = notes
+		@id = id
+	end
+
+
+
+end
--- test.rb
+++ test.rb
@@ -1,17 +1,28 @@
 require 'test/unit'
 
-#require_relative 'crm_'
+require_relative 'processor'
+require_relative 'contact'
 
 class TestCrm < Test::Unit::TestCase
 
 	def setup
-	  @b = Processor.new
-	  @contacts = []
-	  @contacts << Database.new("Stein", "Wang","abc@abc.com", "Great guy", "1001" )
+	  # @b = Processor.new
+   @a = Contact.new("Stein", "Wang","abc@abc.com", "Great guy", "1001" )
+	  #@b.contacts << @contact
 	end
 
-	def test_add_contact
-	  assert_equal @contacts, ["Stein", "Wang","abc@abc.com", "Great guy", "1001" ]
-	end
+  def test_initialize
+    assert_equal "Stein", @a.first_name
+    assert_equal "Wang", @a.last_name
+    assert_equal "abc@abc.com", @a.email
+    assert_equal "Great guy", @a.notes
+    assert_equal "1001", @a.id
+
+  end
+
+	# def test_add_contact
+	#   assert_equal @b.contacts[0], @contact
+
+	# end
 
 end
